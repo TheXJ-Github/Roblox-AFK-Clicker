ea2226fed8134864ab333cdb74494ef18473bdeb
the_xj <147777028+TheXJ-Github@users.noreply.github.com>
2024-05-11 10:39:54 -0700

    added the source code itself
    title

---
+++ AFK Clicker/AFK Clicker/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace AFK_Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.upDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonClick = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.upDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // upDown
+            // 
+            this.upDown.Location = new System.Drawing.Point(12, 101);
+            this.upDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.upDown.Name = "upDown";
+            this.upDown.Size = new System.Drawing.Size(120, 20);
+            this.upDown.TabIndex = 0;
+            this.upDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Distance to deactivate auto-clicker (min 1, max 100)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(353, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "How does this Auto-Clicker work?                     to learn the inner-workings";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(341, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "How do you use this Auto-Clicker?                    to learn how to use this!";
+            // 
+            // buttonClick
+            // 
+            this.buttonClick.Location = new System.Drawing.Point(12, 173);
+            this.buttonClick.Name = "buttonClick";
+            this.buttonClick.Size = new System.Drawing.Size(169, 23);
+            this.buttonClick.TabIndex = 4;
+            this.buttonClick.Text = "Begin Clicking!";
+            this.buttonClick.UseVisualStyleBackColor = true;
+            this.buttonClick.Click += new System.EventHandler(this.buttonClick_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Note that this button will move your";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(205, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "cursor rightward and downward 150 pixels";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(344, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Press F6 to enable/disable the autoclicker. This won\'t move your cursor";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(177, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(56, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Click Here";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.TryVisitLink);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(177, 31);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(56, 13);
+            this.linkLabel2.TabIndex = 9;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Click Here";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.ShowHelp);
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(404, 243);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonClick);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.upDown);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Roblox AFK Clicker - Prevent Disconnecting";
+            ((System.ComponentModel.ISupportInitialize)(this.upDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown upDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonClick;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ AFK Clicker/AFK Clicker/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using Point = System.Drawing.Point;
+
+/* Credits to these cool people making this project possible!
+ * 
+ * Bojidar Qnkov : Mouse Input code
+ * CYBER SOLDIER : DLL Imports
+ * jarvisniu     : Background key listener
+ * 
+ * Resources used for this project
+ * programs
+ * Visual Studio 2019 : Literally everything
+ * Paint.NET : The Icon
+ * 
+ * websites
+ * https://www.codeproject.com/Articles/5264831/How-to-Send-Inputs-using-Csharp
+ * https://github.com/jarvisniu/KeyListener
+ */
+
+namespace AFK_Clicker
+{   
+    public partial class Form1 : Form
+    {
+        // dlls
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, Input[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetMessageExtraInfo();
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        // flags
+        [Flags]
+        public enum InputType
+        {
+            Mouse = 0,
+            Keyboard = 1,
+            Hardware = 2
+        }
+        [Flags]
+
+        public enum KeyEventF
+        {
+            KeyDown = 0x0000,
+            ExtendedKey = 0x0001,
+            KeyUp = 0x0002,
+            Unicode = 0x0004,
+            Scancode = 0x0008
+        }
+        [Flags]
+
+        public enum MouseEventF
+        {
+            Absolute = 0x8000,
+            HWheel = 0x01000,
+            Move = 0x0001,
+            MoveNoCoalesce = 0x2000,
+            LeftDown = 0x0002,
+            LeftUp = 0x0004,
+            RightDown = 0x0008,
+            RightUp = 0x0010,
+            MiddleDown = 0x0020,
+            MiddleUp = 0x0040,
+            VirtualDesk = 0x4000,
+            Wheel = 0x0800,
+            XDown = 0x0080,
+            XUp = 0x0100
+        }
+
+        // structures
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MouseInput
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct KeyboardInput
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HardwareInput
+        {
+            public uint uMsg;
+            public ushort wParamL;
+            public ushort wParamH;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct InputUnion
+        {
+            [FieldOffset(0)] public MouseInput mi;
+            [FieldOffset(0)] public KeyboardInput ki;
+            [FieldOffset(0)] public HardwareInput hi;
+        }
+
+        public struct Input
+        {
+            public int type;
+            public InputUnion u;
+        }
+
+        // begin everything else
+
+        int Maximum_Cursor_Offset = 1;
+        Point Activation_Position = new Point(0, 0);
+
+        /// <summary>
+        /// Gets the position of the user's cursor
+        /// </summary>
+        /// <returns>Point lpPoint</returns>
+        public static Point GetCursorPosition()
+        {
+            return Cursor.Position;
+        }
+
+        /// <summary>
+        /// Moves the cursor X and Y pixels
+        /// </summary>
+        /// <param name="lpPointShift"></param>
+        public static void MoveCursor(Point lpPointShift)
+        {
+            Point cursorPoint = GetCursorPosition();
+            Point newCursorPoint = new Point(cursorPoint.X + lpPointShift.X, cursorPoint.Y + lpPointShift.Y) ;
+
+            Cursor.Position = newCursorPoint;
+        }
+
+        public static bool IsCursorBeyondOffset(Point activatePoint, int maxOffset)
+        {
+            if ((GetCursorPosition().X) < (activatePoint.X - maxOffset))
+            {
+                return true;
+            }
+            if ((GetCursorPosition().X) > (activatePoint.X + maxOffset))
+            {
+                return true;
+            }
+
+            if ((GetCursorPosition().Y) < (activatePoint.Y - maxOffset))
+            {
+                return true;
+            }
+
+            if ((GetCursorPosition().Y) > (activatePoint.Y + maxOffset))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void MouseLeftClick()
+        {
+            Input[] inputs = new Input[]
+{
+                new Input
+                {
+                    type = (int) InputType.Mouse,
+                    u = new InputUnion
+                    {
+                        mi = new MouseInput
+                        {
+                            dx = 0,
+                            dy = 0,
+                            dwFlags = (uint)(MouseEventF.Move | MouseEventF.LeftDown),
+                            dwExtraInfo = GetMessageExtraInfo()
+                        }
+                    }
+                },
+                    new Input
+                    {
+                        type = (int) InputType.Mouse,
+                        u = new InputUnion
+                        {
+                            mi = new MouseInput
+                            {
+                                dwFlags = (uint)MouseEventF.LeftUp,
+                                dwExtraInfo = GetMessageExtraInfo()
+                            }
+                        }
+                    }
+                };
+
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+        }
+        bool isClickerActive = false;
+
+        private void AfkClick()
+        {
+            if (isClickerActive == false)
+            {
+                isClickerActive = true;
+
+                Activation_Position = GetCursorPosition();
+                Maximum_Cursor_Offset = (int)upDown.Value;
+
+                GraphicsDrawing.DrawRectangleOutline(Activation_Position, Maximum_Cursor_Offset);
+
+                do
+                {
+                    Thread.Sleep(1000);
+                    MouseLeftClick();
+                    GraphicsDrawing.DrawRectangleOutline(Activation_Position, Maximum_Cursor_Offset);
+                }
+                while (!IsCursorBeyondOffset(Activation_Position, Maximum_Cursor_Offset));
+
+                GraphicsDrawing.ClearDrawings();
+                MessageBox.Show("The autoclicker has been turned off", "Roblox AFK Clicker");
+                isClickerActive = false;
+            }
+        }
+
+        private void KeyPressed()
+        {
+            if (isClickerActive == false)
+            {
+                AfkClick();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            KeyListener k = new KeyListener();
+            k.onPress("F6", KeyPressed);
+        }
+
+        private void buttonClick_Click(object sender, EventArgs e)
+        {
+            MoveCursor(new Point(150, 150));
+            AfkClick();
+        }
+
+        private void TryVisitLink(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://github.com/TheXJ-Github/RobloxAfkClicker");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link!\n" + ex, "Roblox AFK Clicker");
+            }
+        }
+
+        private void ShowHelp(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show(
+                 "To activate the auto-clicker, you would either:"
+                + "\nPress the 'Begin Clicking!' button"
+                + "\nPress the F6 key"
+                + "\n"
+                + "\nWhen you do this, a red square will appear centered where your cursor is"
+                + "\nWhat does the red square mean? It's how far your cursor can move without turning off the auto-clicker."
+                + "\n" +
+                "\nYou can customize the size of the red square, the option is above the button.",
+                "Roblox AFK Clicker");
+        }
+    }
+
+    public class GraphicsDrawing
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("User32.dll")]
+        private static extern int ReleaseDC(IntPtr hWnd, IntPtr dc);
+
+        [DllImport("user32.dll")]
+        private static extern bool InvalidateRect(IntPtr hWnd, IntPtr lpRect, bool bErase);
+
+        public static void DrawRectangleOutline(Point lpCenter, int lpSize)
+        {
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            System.Drawing.Graphics g = System.Drawing.Graphics.FromHdc(hdc);
+
+            Pen pen = new Pen(Color.Red);
+
+            // points for drawing
+            Point p1 = new Point(lpCenter.X - lpSize, lpCenter.Y - lpSize);
+            Point p2 = new Point(lpCenter.X - lpSize, lpCenter.Y - lpSize);
+
+            // left
+            p2.Y += (lpSize * 2);
+            g.DrawLine(pen, p1, p2);
+
+            // bottom
+            p1.Y += (lpSize * 2);
+            p2.X += (lpSize * 2);
+            g.DrawLine(pen, p1, p2);
+
+            // right
+            p1.X += (lpSize * 2);
+            p2.Y -= (lpSize * 2);
+            g.DrawLine(pen, p1, p2);
+
+            // top
+            p1.Y -= (lpSize * 2);
+            p2.X -= (lpSize * 2);
+            g.DrawLine(pen, p1, p2);
+
+            ReleaseDC(IntPtr.Zero, hdc);
+        }
+
+        public static void ClearDrawings()
+        {
+            InvalidateRect(IntPtr.Zero, IntPtr.Zero, false);
+        }
+    }
+}
